6c1db66763a4dfc9c2289a96f83095248d974951
pancreasio <julianholisu@gmail.com>
2020-07-29 07:37:05 -0300

    basic movement implemented

--- Toadder/Assets/Scripts/FrogController.cs
+++ Toadder/Assets/Scripts/FrogController.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class FrogController : MonoBehaviour
 {
+    private float travelDistance;
     // Start is called before the first frame update
     void Start()
     {
-        
+        travelDistance = LevelData.LevelInstance.GetComponent<Grid>().cellSize.x *
+                         LevelData.LevelInstance.transform.localScale.x;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+            transform.Translate(Vector3.up * travelDistance);
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+            transform.Translate(Vector3.up * -travelDistance);
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+            transform.Translate(Vector3.right * travelDistance);
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+            transform.Translate(Vector3.right * -travelDistance);
     }
 }
---
+++ Toadder/Assets/Scripts/LevelData.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+public class LevelData : MonoBehaviour
+{
+    public static GameObject LevelInstance;
+
+    public Grid levelGrid;
+
+    void Awake()
+    {
+        if (LevelInstance == null)
+            LevelInstance = this.gameObject;
+        else
+            Destroy(this.gameObject);
+    }
+}
